2fb31c89f2e359f76c95a8769918079ae44d2c50
mayank dubey <dubeymayank770@gmail.com>
2024-07-24 11:57:46 +0530

    Array mein naam hai

---
+++ ArrayDemo.java
@@ -0,0 +1,16 @@
+package DS_Java_Training;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        String[] name = new String[5];
+        name = new String[]{"wasif" , "harsh" ,"mayank","rahul","yasharth"};
+        for (int i = 0; i < name.length; i++) {
+            System.out.println(name[i]);
+        }
+
+
+    }
+
+
+}
