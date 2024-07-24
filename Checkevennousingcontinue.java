6163eff2660215c8faff5951908252e2a1a0e018
mayank dubey <dubeymayank770@gmail.com>
2024-07-24 11:40:56 +0530

    checking even no using continue

---
+++ Checkevennousingcontinue.java
@@ -0,0 +1,19 @@
+package DS_Java_Training;
+
+public class Checkevennousingcontinue {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            if (i%2 == 0){
+                System.out.println(i);
+            } else {
+                continue;
+
+            }
+
+
+        }
+
+
+
+    }
+}
